57f49b7cb35f7a869fedb67455fae80f720a11b5
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-01 00:40:32 +0530

    3b all done

---
+++ 3b 2.py
@@ -0,0 +1,7 @@
+inm = int(input("Enter the number of rows : "))
+nm = 1
+for i in range(1,inm+1):
+    for j in range(1,i+1):
+        print(nm,end='')
+        nm = nm+1
+    print()
---
+++ 3b 3.py
@@ -0,0 +1,9 @@
+n =  int(input("Enter the number : "))
+for i in range(1,n+1):
+    for j in range(1,n+1-i):
+        print(" ",end='')
+    for j in range(i,0,-1):
+        print(j,end='')
+    for j in range(2,i+1):
+        print(j,end='')
+    print()
