be55d76bef31ad862139c1c7fd64e532e4e403a9
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-01 11:56:59 +0530

    done with the collage assignment

--- 3b 2.py
+++ 3b 2.py
@@ -2,6 +2,6 @@ inm = int(input("Enter the number of rows : "))
 nm = 1
 for i in range(1,inm+1):
     for j in range(1,i+1):
-        print(nm,end='')
+        print(nm,end=' ')
         nm = nm+1
     print()
