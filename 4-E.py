6bbd3d8a3679bb11ba21334c6f6790fc891de926
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-08 22:09:34 +0530

    done with 4-e

---
+++ 4-E.py
@@ -0,0 +1,22 @@
+d = {'jan':52,'feb':52,'march':52,'april':34,'may':76,'june':45,'july':22,'august':88,'sep':45}
+#print(d.keys()[0])
+
+print(d.keys())
+
+print(d.values())
+print(len(d.values()))
+
+l = list(d.values())
+print(l)
+from collections import Counter
+df = Counter(l)
+m = len(l)
+n = []
+for i in range(m):
+    for j in range(i+1,m):
+        if l[i]==l[j]:
+            l[j] = 0
+
+    if l[i]!=0:
+        n.append(l[i])
+print(n)
